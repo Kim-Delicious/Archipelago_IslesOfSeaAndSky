834e73cdc2d02724bbfa7584f5de8c375ff5af73
Kimber-Delicious <clifton.kimber@gmail.com>
2026-05-03 23:11:54 -0600

    APWORLD: fix error. Also add a TODO with the intention of documenting altered files in the patch.

--- worlds/isles_of_sea_and_sky/Client.py
+++ worlds/isles_of_sea_and_sky/Client.py
@@ -455,7 +455,7 @@ async def multi_watcher(ctx: IslesOfSeaAndSkyContext):
 async def game_watcher(ctx: IslesOfSeaAndSkyContext):
     while not ctx.exit_event.is_set():
 
-        if ctx.slot is None
+        if ctx.slot is None:
             await asyncio.sleep(0.1)
             continue
         
